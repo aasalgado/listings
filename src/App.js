7850d38d284296d820b13f886004c293e3eab7bf
Anthony Salgado <anthonyasalgado@gmail.com>
2021-03-16 15:27:42 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { initListings } from './redux/listingsReducer';
 function App() {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(initListings());
+  }, [dispatch]);
   return (
     <div className="App">
       Hello There!
