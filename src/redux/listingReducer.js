8f0ba22ad14f300133ff709c21dfbbb341bd3cc7
Anthony Salgado <anthonyasalgado@gmail.com>
2021-03-17 15:50:55 -0700

    included the reducer and the action function in listingReducer.js

---
+++ src/redux/listingReducer.js
@@ -0,0 +1,20 @@
+import listingsService from '../services/listingsService';
+
+const listingsReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'INIT_LISTINGS':
+            return action.data;
+        default:
+            return state;
+    }
+};
+export const initListings = () => {
+    return async dispatch => {
+       const listings = await listingsService.getAll(); 
+       dispatch({
+           type: 'INIT_LISTINGS',
+           data: listings,
+       });
+    };
+};
+export default listingsReducer;
